7f0d8b2d0eccfd01cab0f6151cb22451028b02a2
Yeon-Hyeong Yang <lbird94@gmail.com>
2019-05-09 13:20:59 +0900

    Fix raindrop.sh to match latest amocil arg format

--- raindrop.sh
+++ raindrop.sh
@@ -4,7 +4,7 @@ TESTER0=BC4BAF38355C6CCF8422DD3D273B3DBB83B2370B
 TESTER1=61D6C533B93B5BD7F9B6A11C5356C484D17692BC
 
 while true; do
-	amocli --rpc $RPC tx transfer --user tester1 --to $TESTER0 --amount 100
-	amocli --rpc $RPC tx transfer --user tester0 --to $TESTER1 --amount 100
+	amocli --rpc $RPC tx transfer --user tester1 $TESTER0 100
+	amocli --rpc $RPC tx transfer --user tester0 $TESTER1 100
 	sleep 600
 done
