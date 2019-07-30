e7160b8b029fefe24ef0ec2abaa828789c06640e
Yeon-Hyeong Yang <lbird94@gmail.com>
2019-07-30 17:50:01 +0900

    Read moniker name from config file

--- DOCKER_amod/run_node.sh
+++ DOCKER_amod/run_node.sh
@@ -4,12 +4,9 @@ fi
 if [ -z "$TMHOME" ]; then
 	TMHOME=/tendermint
 fi
-if [ -z "$MONIKER" ]; then
-	MONIKER=default
-fi
 
 /usr/bin/tendermint --home $TMHOME init
 
 /usr/bin/amod run &
-/usr/bin/tendermint --moniker $MONIKER --home $TMHOME node
+/usr/bin/tendermint --home $TMHOME node
 
