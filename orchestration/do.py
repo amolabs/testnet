cc31b444feeae6d8b04575aee4958d450f051850
AHN Jaehong <monsieur.ahn@gmail.com>
2019-11-11 14:24:43 +0900

    Support node orchestration in parallel mode
    - minor changes on config.json

--- orchestration/do.py
+++ orchestration/do.py
@@ -6,7 +6,8 @@ import time
 import subprocess
 import json
 
-import paramiko
+from pssh.clients import ParallelSSHClient
+from gevent import joinall
 
 CURRENT_PATH = os.getcwd()
 ORCH_PATH = CURRENT_PATH + "/orchestration"
@@ -24,18 +25,54 @@ AMOCLI = AMOCLICMD + ' ' + AMOCLIOPT
 
 ONEAMO = 1000000000000000000
 
-def all_up(ssh, amo, nodes, only_boot):
+#########################
+# Support Parallel Mode #
+#########################
+# * non parallel        #
+#   - all_faucet_stake  #
+# * half parallel       #
+#   - all_setup         #
+#     + val1            #
+#     + seed(dep)       #
+#     + val...(dep)     #
+# * full parallel       #
+#   - all_up            #
+#   - all_down          #
+#########################
+
+def all_up(ssh, amo, nodes):
     nodes = {**nodes}
-    rpc_addr = nodes["val1"]["ip_addr"] + ":26657"
     image_version = amo["image_version"] 
+    
+    # seed, val... : parallel
+    print("bootstrap nodes")
+    bootstrap(ssh, nodes, image_version)
+
+    print()
+    nodes.clear()
+
+def all_down(ssh, nodes):
+    nodes = {**nodes}
+
+    # seed, val... : parallel
+    print("stop nodes")
+    docker_stop(ssh, nodes)
+    
+    print()
+    nodes.clear()
+
+def all_faucet_stake(ssh, amo, nodes):
+    nodes = {**nodes}
+   
+    rpc_addr = nodes["val1"]["ip_addr"] + ":26657"
     amount = 100 * ONEAMO
 
-    target = "val1"
-    node = nodes[target]
-    bootstrap(ssh, node, image_version, target)
-    if not only_boot:
-        valkey_path = os.path.join(DATA_PATH, target,
-                'priv_validator_key.json')
+    if "seed" in nodes:
+        del nodes["seed"]
+
+    # val... : non-parallel
+    for target, node in nodes.items():
+        valkey_path = os.path.join(DATA_PATH, target, 'priv_validator_key.json')
         val_pubkey = None
         if os.path.isfile(valkey_path):
             with open(valkey_path) as file:
@@ -43,84 +80,59 @@ def all_up(ssh, amo, nodes, only_boot):
                 val_pubkey = valkey['pub_key']['value']
         if val_pubkey is None:
             return
+
         print("faucet to %s: %d" % (target, amount)) 
         transfer(rpc_addr, amo["faucet_user"], node["amo_addr"], amount) 
         print("stake for %s: %d" % (target, amount)) 
         stake(rpc_addr, target, val_pubkey, amount)
-    del nodes[target]
 
-    target = "seed"
-    if target in nodes:
-        node = nodes[target]
-        bootstrap(ssh, node, image_version, target)
-        del nodes[target]
-
-    for target, node in nodes.items():        
-        bootstrap(ssh, node, image_version, target)
-        if not only_boot:
-            valkey_path = os.path.join(DATA_PATH, target,
-                    'priv_validator_key.json')
-            val_pubkey = None
-            if os.path.isfile(valkey_path):
-                with open(valkey_path) as file:
-                    valkey = json.load(file)
-                    val_pubkey = valkey['pub_key']['value']
-            if val_pubkey is None:
-                return
-            print("faucet to %s: %d" % (target, amount))
-            transfer(rpc_addr, amo["faucet_user"], node["amo_addr"], amount) 
-            print("stake for %s: %d" % (target, amount))
-            stake(rpc_addr, target, val_pubkey, amount) 
-
-    nodes.clear()
-
-def all_down(ssh, nodes):
-    nodes = {**nodes}
-
-    target = "val1"
-    docker_stop(ssh, nodes[target], target)
-    del nodes[target]
-
-    target = "seed"
-    if target in nodes:
-        docker_stop(ssh, nodes[target], target)
-        del nodes[target]
-
-    for target in list(nodes.keys()):        
-        docker_stop(ssh, nodes[target], target)
-    
+    print()
     nodes.clear()
 
 def all_setup(ssh, amo, nodes):
     nodes = {**nodes}
 
+    print("setup nodes")
+        
+    # transfer to seed, val... : parallel
+    transfer_config(ssh, amo, nodes) 
+
+    # val1 : non-parallel
     target = "val1"
-    node = nodes[target]
-    node_id = setup_node(ssh, amo, node, target, "''")
-    peer = node_id + "@" + node["ip_addr"] + ":" + str(node["p2p_port"])
-    #print(peer)
+    ssh.hosts = [nodes[target]["ip_addr"]]
+    nodes_tmp = {target:nodes[target]}
+
+    node_id = setup_node(ssh, amo, nodes_tmp, "''")
+    peer = node_id + "@" + nodes[target]["ip_addr"] + ":" + str(amo["p2p_port"])
+
     del nodes[target] # exclude from loop
 
+    # seed : non-parallel
     target = "seed"
     if target in nodes:
-        node = nodes[target]
-        node_id = setup_node(ssh, amo, node, target, peer)
-        peer = node_id + "@" + node["ip_addr"] + ":" + str(node["p2p_port"])
+        ssh.hosts = [nodes[target]["ip_addr"]]
+        nodes_tmp = {target:nodes[target]}
+
+        node_id = setup_node(ssh, amo, nodes_tmp, peer)
+        peer = node_id + "@" + nodes[target]["ip_addr"] + ":" + str(amo["p2p_port"])
         del nodes[target] # exclude from loop
 
-    for target in list(nodes.keys()):        
-        node = nodes[target]
-        setup_node(ssh, amo, node, target, peer)
+    # val... : parallel
+    ssh.hosts = [h["ip_addr"] for h in nodes.values()]
 
+    setup_node(ssh, amo, nodes, peer)
+
+    print()
     nodes.clear()
 
-def all_exec(ssh, amo, nodes, exec_cmd):
-    for name, node in nodes.items():
-        print("[%s] connecting to %s" % (name, node['ip_addr']))
-        ssh = ssh_connect(ssh, node['ip_addr'], node['username'])
-        print("[%s] executing '%s'" % (name, exec_cmd))
-        out, _ = ssh_exec(ssh, exec_cmd)
-        print(''.join(out))
+def all_exec(ssh, exec_cmd):
+    # seed, val... : parallel
+    ssh_exec(ssh, exec_cmd, wait=True, echo=True)
+
+def all_scp(ssh, local_path, remote_path):
+    # seed, val... : parallel
+    greenlets = ssh_transfer(ssh, local_path, remote_path)
+    joinall(greenlets) # wait until transfer is done
 
 def amocli_exec(tx_type, rpc_addr, username, dest_addr, amount):
     try:
@@ -146,164 +158,191 @@ def transfer(rpc_addr, username, to_addr, amount):
     result = amocli_exec("transfer", rpc_addr, username, to_addr, amount)
     print(result.decode('utf-8'))
 
-def bootstrap(ssh, node, image_version, target):
+def bootstrap(ssh, nodes, image_version):
     try:
-        print("[%s] bootstrap node" % (target))
-        
-        target_ip = node["ip_addr"]
+        host_args = get_host_args(ssh.hosts, nodes) 
 
-        print("[%s] connecting to %s" % (target, target_ip))
-        ssh = ssh_connect(ssh, target_ip, node["username"])
-        print("[%s] connected to %s" % (target, target_ip))
+        print("execute 'run.sh' script", end='', flush=True)
+        command = "./orchestration/run.sh /testnet/%(target)s/ " + image_version
+        ssh_exec(ssh, command, host_args=host_args, wait=True)
+        print(" - DONE")
 
-        print("[%s] execute 'run.sh' script" % (target))
-        command = "sudo ./orchestration/run.sh /testnet/%s/ %s" % (target, image_version)
-        ssh_exec(ssh, command)
-    
-        print("[%s] check docker status" % (target))
-        comman = "sudo docker inspect " + target
-        ssh_exec(ssh, command)
+        print("check docker status", end='', flush=True)
+        comman = "docker inspect %(target)s"
+        ssh_exec(ssh, command, host_args=host_args, wait=True)
+        print(" - DONE")
 
-        print("[%s] check rpc connection" % (target), end = ' ', flush = True)
+        print("check rpc connection ", end='', flush=True)
         check_status(ssh)
 
     except Exception as err:
-        print("[%s] %s" % (target, err))
-        ssh.close()
+        print(err)
         exit(1)
 
-    finally:
-        ssh.close()
-
-def docker_stop(ssh, node, target):
+def docker_stop(ssh, nodes):
     try:
-        target_ip = node["ip_addr"]
+        host_args = get_host_args(ssh.hosts, nodes) 
 
-        print("[%s] connecting to %s" % (target, target_ip))
-        ssh = ssh_connect(ssh, target_ip, node["username"])
-        print("[%s] connected to %s" % (target, target_ip))
-
-        print("[%s] stop docker container" % (target))
-        command = "sudo docker stop %s" % (target)
-        ssh_exec(ssh, command)
+        print("stop docker container:", ssh.hosts, end='', flush=True)
+        command = "docker stop %(target)s"
+        ssh_exec(ssh, command, host_args=host_args, wait=True)
+        print(" - DONE")
 
     except Exception as err:
-        print("[%s] %s" % (target, err))
-        ssh.close()
+        print(err)
         exit(1)
 
-    finally:
-        ssh.close()
+def transfer_config(ssh, amo, nodes):
+    try:
+        orch_remote_path = "orchestration/"
+        docker_image = IMAGE_NAME + ":" + amo["image_version"]
+       
+        genesis_source_path = amo["genesis_file"]
+        genesis_dest_path = os.path.join(CURRENT_PATH, "genesis.json")
 
-def setup_node(ssh, amo, node, target, peer):
+        command = "ln -sf %s %s" % (genesis_source_path, genesis_dest_path)
+        proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        out, err = proc.communicate()
+
+        if err: raise Exception(err)
+
+        command = "docker pull %s" % (docker_image)
+        print(command + ":", ssh.hosts, end='', flush=True)
+        ssh_exec(ssh, command, wait=True)
+        print(" - DONE")
+
+        print("prepare config files to transfer:", ssh.hosts, end='', flush=True)
+        for f in os.listdir(CURRENT_PATH):
+            if not os.path.isfile(os.path.join(CURRENT_PATH, f)):
+                continue
+            if not f.endswith(".sh") and not f.endswith(".in") and not f.endswith(".json"):
+                continue
+
+            for target in nodes.keys():
+                from_path = os.path.join(CURRENT_PATH, f)
+                to_path = os.path.join(DATA_PATH + "/" + target + "/", f)
+
+                command = "cp %s %s" % (from_path, to_path)
+                proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+                out, err = proc.communicate()
+       
+                if err: raise Exception(err)
+
+        print(" - DONE")
+
+        hosts_bk = ssh.hosts
+
+        greenlets = []
+
+        print("transfer config files to nodes:", ssh.hosts, end='', flush=True)
+        for target in nodes.keys():
+            ssh.hosts = [nodes[target]["ip_addr"]]
+
+            local_path = os.path.join(DATA_PATH, target)
+            remote_path = orch_remote_path
+           
+            greenlet = ssh_transfer(ssh, local_path, remote_path)[0]
+            greenlets.append(greenlet)
+
+        joinall(greenlets, raise_error=True)
+        print(" - DONE")
+
+        ssh.hosts = hosts_bk 
+
+        print("set execution permission on script files:", ssh.hosts, end='', flush=True)
+        command = "chmod +x ./orchestration/*.sh"
+        ssh_exec(ssh, command, wait=True)
+        print(" - DONE")
+    
+    except Exception as err:
+        print(err)
+        exit(1)
+
+def setup_node(ssh, amo, nodes, peer):
     node_id = ''
     try:
-        print("[%s] setting up node" % (target))
+        orch_remote_path = "orchestration"
+        host_args = get_host_args(ssh.hosts, nodes) 
 
-        target_ip = node["ip_addr"]
+        print("execute 'setup.sh' script:", ssh.hosts, end='', flush=True)
+        command = "cd " + orch_remote_path + "; ./setup.sh -e %(ip)s /testnet/%(target)s/ %(target)s " + peer
+        output = ssh_exec(ssh, command, host_args=host_args, wait=True)
+        print(" - DONE")
 
-        print("[%s] connecting to %s" % (target, target_ip))
-        ssh = ssh_connect(ssh, target_ip, node["username"])
-        print("[%s] connected to %s" % (target, target_ip))
-
-        command = "pwd"
-        output, _ = ssh_exec(ssh, command)
-        orch_remote_path = output.readlines()[0].strip() + "/orchestration"
-        print("[%s] creating remote path: %s" % (target, orch_remote_path))
-        command = "mkdir -p %s" % orch_remote_path
-        ssh_exec(ssh, command)
-
-        docker_image = IMAGE_NAME + ":" + amo["image_version"]
-
-        print("[%s] docker pull %s" % (target, docker_image))
-        command = "sudo docker pull %s" % (docker_image)
-        ssh_exec(ssh, command)
-
-        print("[%s] scp general config files" % (target))
-        for f in os.listdir(CURRENT_PATH):
-            local_path = os.path.join(CURRENT_PATH, f)
-            remote_path = os.path.join(orch_remote_path, f)
-            
-            if os.path.isfile(local_path) and \
-               (local_path.endswith(".sh") or \
-                local_path.endswith(".in") or \
-                local_path.endswith(".json")):
-
-                ssh_transfer(ssh, local_path, remote_path)
-                
-                if local_path.endswith(".sh"):
-                    command = "sudo chmod +x %s" % (remote_path)
-                    ssh_exec(ssh, command)
-
-        print("[%s] scp private config files" % (target))
-        for f in os.listdir(DATA_PATH + "/" + target + "/"):
-            local_path = os.path.join(DATA_PATH + "/" + target + "/", f)
-            remote_path = os.path.join(orch_remote_path, f)
-
-            if os.path.isfile(local_path):
-                ssh_transfer(ssh, local_path, remote_path)
-
-        genesis_file = amo["genesis_file"]
-
-        print("[%s] scp %s file" % (target, genesis_file))
-        local_path = os.path.join(CURRENT_PATH, genesis_file)
-        remote_path = os.path.join(orch_remote_path, "genesis.json")
-        ssh_transfer(ssh, local_path, remote_path)
-
-        print("[%s] execute 'setup.sh' script" % (target))
-        command = "cd %s; sudo ./setup.sh -e %s /testnet/%s/ %s %s" % \
-                (orch_remote_path, target_ip, target, target, peer)
-        output, _ = ssh_exec(ssh, command)
-        output = output.readlines()
-        node_id = output[len(output)-1].strip()
+        if len(ssh.hosts) == 1:
+            output = list(output[ssh.hosts[0]].stdout)
+            node_id = output[len(output)-1].strip()
 
     except Exception as err:
-        print("[%s] %s" % (target, err))
-        ssh.close()
+        print(err)
         exit(1)
 
     finally:
-        ssh.close()
         return node_id
         
 def check_status(ssh):
-    command = "curl localhost:26657/status"
-    result, _ = ssh_exec(ssh, command)
-    while(len(result.read()) == 0):
-        print(".", end = ' ', flush = True)
-        result, _ = ssh_exec(ssh, command)
+    ok_num = 0
+
+    while ok_num < len(ssh.hosts):
+        ok_num = 0
+        
+        command = "curl localhost:26657/status"
+        output = ssh_exec(ssh, command)
+        
+        for host_output in output.values():
+            host_output = list(host_output.stdout)
+            if len(host_output) > 0:
+                ok_num += 1
+
         time.sleep(SLEEP_TIME)
+        print(".", end = '', flush = True)
     
-    print("FULLY UP!")
+    print(" - FULLY UP!")
+
+def get_host_args(hosts, nodes):
+    host_args = []
+    for host in hosts:
+        target = None
+        for node, config in nodes.items():
+            if config["ip_addr"] == host:
+                target = node
+
+        if target == None:
+            continue
+
+        host_args.append({"target": target, "ip": host})
+
+    return host_args
 
 def ssh_transfer(ssh, local_path, remote_path):
     try:
-        t = ssh.open_sftp()
-        t.put(local_path, remote_path)
-    except Exception as err:
-        print(err)
-        ssh.close()
-        exit(1)
-
-def ssh_exec(ssh, command):
-    try:
-        stdin, stdout, stderr = ssh.exec_command(command)
-
+        # return greenlets
+        return ssh.scp_send(local_path, remote_path, recurse=True)
     except Exception as err:
         print(err)
         exit(1)
 
-    return stdout, stdout.channel.recv_exit_status()
-
-def ssh_connect(ssh, hostname, username):
+def ssh_exec(ssh, command, host_args=None, wait=False, echo=False):
     try:
-        ssh.connect(hostname, username=username,
-                key_filename=config['client']['ssh_key_path'])
-    except Exception as error:
-        print(error)
-        exit(1)
+        output = ssh.run_command(
+                command, 
+                host_args=host_args, 
+                sudo=True, 
+                stop_on_errors=True)
 
-    return ssh
+        if wait == True: 
+            ssh.join(output) # wait until all nodes' job end
+
+        if echo == True:
+            for host, host_output in output.items():
+                for line in host_output.stdout:
+                    print("host=%s, command=%s, output=%s" % (host, command, line))
+
+        return output
+
+    except Exception as err:
+        print(err)
+        exit(1)
 
 def main():
     global config
@@ -322,40 +361,56 @@ def main():
     else:
         client = {"ssh_key_path": DEFAULT_KEY_PATH}
 
-    config["client"] = client
-
     nodes = config["nodes"]
     amo = config["amo"]
 
     # prepare ssh module
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    hosts = [h["ip_addr"] for h in nodes.values()]
+    ssh = ParallelSSHClient(
+            hosts=hosts, 
+            user=client["ssh_username"],
+            pkey=client["ssh_key_path"],
+            allow_agent=False)
 
     cmd = sys.argv[1]
     
     if cmd == "init":
-        all_up(ssh, amo, nodes, only_boot=False)
+        all_up(ssh, amo, nodes)
+        all_faucet_stake(ssh, amo, nodes)
     elif cmd == "up": 
-        all_up(ssh, amo, nodes, only_boot=True)
+        all_up(ssh, amo, nodes)
     elif cmd == "down": 
         all_down(ssh, nodes)
+    elif cmd == "restart":
+        all_down(ssh, nodes)
+        ssh.hosts = hosts # reset
+        all_up(ssh, amo, nodes)
     elif cmd == "setup":
         all_setup(ssh, amo, nodes)
     elif cmd == "reset":
         all_down(ssh, nodes)
+        ssh.hosts = hosts # reset
         all_setup(ssh, amo, nodes)
-        all_up(ssh, amo, nodes, only_boot=False)
+        ssh.hosts = hosts # reset
+        all_up(ssh, amo, nodes)
+        ssh.hosts = hosts # reset
+        all_faucet_stake(ssh, amo, nodes)
+    elif cmd == "scp":
+        if len(sys.argv) == 4:
+           all_scp(ssh, sys.argv[2], sys.argv[3]) 
+        else:
+            usage()
     elif cmd == "exec":
         # TODO: use getopt
         if len(sys.argv) >= 3:
-            all_exec(ssh, amo, nodes, sys.argv[2])
+            all_exec(ssh, sys.argv[2])
         else:
             usage()
     else:
         usage()
 
 def usage():
-    print("Usage: python3 %s { init | up | down | setup | reset }" % (sys.argv[0]))
+    print("Usage: python3 %s { init | up | down | restart | setup | reset | exec | scp }" % (sys.argv[0]))
 
 if __name__ == "__main__":
     if len(sys.argv) < 2: 
