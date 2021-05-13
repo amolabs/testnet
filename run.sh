9be18df9ee25924d179d27563054b05db0577a8d
Yeon-Hyeong Yang <lbird94@gmail.com>
2021-05-13 17:41:05 +0900

    Add tty and port control to run.sh

--- run.sh
+++ run.sh
@@ -2,19 +2,29 @@
 
 DOCKER=false
 IMAGE_VERSION=latest
+PUBLISH="-p 26657"
+TTY="-it"
 
 usage() {
 	echo "syntax: $0 [options] <data_root>"
 	echo "options:"
 	echo "  -d  use docker with specified image version"
+	echo "  -p  publish ports 26656,26657,26660"
+	echo "  -s  keep silent, i.e. do not attach stdio"
 	echo "  -h  print usage"
 }
 
-while getopts "d:h" arg; do
+while getopts "d:psh" arg; do
 	case $arg in
 		d)
 			DOCKER=true
 			IMAGE_VERSION=$OPTARG
+			;;
+		p)
+			PUBLISH="-p 26656-26657 -p 26660"
+			;;
+		s)
+			TTY="-d"
 			;;
 		h | *)
 			usage
@@ -35,7 +45,7 @@ echo "data root     = $DATAROOT"
 
 if [ "$DOCKER" = true ]; then
 	echo "docker image  = $IMAGE_VERSION"
-	docker run -it --rm -p 26657 -v $DATAROOT/amo:/amo amolabs/amod:$IMAGE_VERSION amod run --home /amo
+	docker run $TTY --rm --name amod $PUBLISH -v $DATAROOT/amo:/amo amolabs/amod:$IMAGE_VERSION amod run --home /amo
 else
 	amod run --home $DATAROOT/amo
 fi
